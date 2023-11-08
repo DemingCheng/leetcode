038afddc342abbdb4350732bca847b3bdbabcda9
deming <deming.cheng@outlook.com>
2023-11-08 17:16:41 +0800

    add

--- hpc/cuda/reduction/reduction.cu
+++ hpc/cuda/reduction/reduction.cu
@@ -2,17 +2,17 @@
 
 const int N = 32*1024*1024;           // 输入矩阵的大小input size 32M的global memory
 const int thread_per_block = 256;    // 每个block的thread数  2^8个
-const int block_num = N / thread_per_block;                
+const int block_num = N / thread_per_block;                  
 
-__global__ void reduce3(float* d_in, float* d_out) {
-    __shared__ float cache[block_num];
-    unsigned int tid = threadIdx.x;
-    unsigned int i = threadIdx.x + blockIdx.x * (blockDim.x * 2);
+// __global__ void reduce3(float* d_in, float* d_out) {
+//     __shared__ float cache[block_num];
+//     unsigned int tid = threadIdx.x;
+//     unsigned int i = threadIdx.x + blockIdx.x * (blockDim.x * 2);
 
-    cache[tid] = d_in[] + ; 
+//     cache[tid] = d_in[] + ; 
 
 
-}
+// }
 
 // warp是并行计算时最小的并发结构，通常由连续32个thread组成。
 // 同一个warp中的所有thread是完全并行的，且必须要执行相同的指令，当同一warp中的thread分配了不同的指令时，会发生warp divergence
@@ -62,27 +62,52 @@ __global__ void reduce1_1(float* d_in, float* d_out) {
     }
 }
 
-__global__ void reduce0(float* d_in, float* d_out) {
+// 最基础reduction
+__global__ void reduce_0(float* d_in, float* d_out) {
     // 分配shared memory ，将数据global load到shared memory
-    __shared__ float cache[thread_per_block];
+    __shared__ float sdata[thread_per_block];
 
-    unsigned int tid = blockIdx.x * blockDim.x + threadIdx.x;
-    unsigned int cacheIndex = threadIdx.x;
+    unsigned int i = threadIdx.x + blockIdx.x * blockDim.x;
+    unsigned int tid = threadIdx.x;  // 块内index
 
     // each thread load one element form global to shared mem
-    cache[cacheIndex] = d_in[tid];
+    sdata[tid] = d_in[i];
     __syncthreads();
 
     // do reduction in each block
-    for(unsigned int i = 1; i < blockDim.x; i *= 2) { // or i < thread_per_block
-        if(cacheIndex % (2 * i) == 0) {
-            cache[cacheIndex] += cache[cacheIndex + i];
-        }     
+    for(unsigned int stride = 1; stride < blockDim.x; stride *= 2) { // or stride < thread_per_block
+        if(tid % (2 * stride) == 0) {
+            sdata[tid] += sdata[tid + stride];
+        }
         __syncthreads();
     }
 
-    if(cacheIndex == 0) {
-        d_out[blockIdx.x] = cache[0]; 
+    if(tid == 0) {
+        d_out[blockIdx.x] = sdata[tid]; 
+    }
+}
+
+__global__ void reduce0(float* d_in,float* d_out){
+    __shared__ float sdata[thread_per_block];
+
+    //each thread loads one element from global memory to shared mem
+    unsigned int i=blockIdx.x*blockDim.x+threadIdx.x;
+    unsigned int tid=threadIdx.x;
+    sdata[tid]=d_in[i];
+    __syncthreads();
+
+    // do reduction in shared mem
+    for(unsigned int s=1; s<blockDim.x; s*=2){
+        if(tid%(2*s) == 0){
+            sdata[tid]+=sdata[tid+s];
+        }
+        __syncthreads();
+    }
+    
+    // write result for this block to global mem
+    if(tid==0) {
+        d_out[blockIdx.x]=sdata[tid];
+        printf("blockIdx.x: %lf", d_out[blockIdx.x]);
     }
 }
 
@@ -119,7 +144,13 @@ int main() {
     dim3 Grid(block_num, 1);           // 一个grid里多少个block
     dim3 Block(thread_per_block, 1);   // 一个block里多少个thread
     // reduce0<<<Grid, Block>>>(d_in, d_out);
-    reduce1_1<<<Grid, Block>>>(d_in, d_out);
+    reduce0<<<Grid, Block>>>(d_in, d_out);
+
+    cudaError_t err = cudaGetLastError();
+    if(err != cudaSuccess) {
+        printf("Failed: Cuda error %s:%d '%s'\n", __FILE__,__LINE__,cudaGetErrorString(err));
+    }
+    
 
     cudaMemcpy(h_out, d_out, block_num * sizeof(float), cudaMemcpyDeviceToHost);
 
@@ -132,15 +163,17 @@ int main() {
         ans[i] = curr;
     }
 
-    if(check(h_out, ans) == false) {
-        for(int i = 0; i < 1024; i++) {
-            if(h_out[i] != ans[i]) {
-                printf("%d, %lf\n", i, h_out[i]);
-            }
-            // printf("%lf", h_out[i]);
-        }
-        printf("\n");
-    }
+    // if(check(h_out, ans) == false) {
+    //     for(int i = 0; i < 1024; i++) {
+    //         if(h_out[i] != ans[i]) {
+    //             printf("%d, %lf\n", i, h_out[i]);
+    //         }
+    //         // printf("%lf", h_out[i]);
+    //     }
+    //     printf("\n");
+    // }
+    
+    printf("check(h_out, ans): %s\n", check(h_out, ans) ? "true": "false");
     printf("block_num: %d\n", block_num);
 
     cudaFree(d_in);
